fef6a1b7a3b08626a37428208158e1a118782a4a
Saiisshh <118446344+saishpawar@users.noreply.github.com>
2023-03-19 19:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+let s=0,e=str.length()-1;
 
+	str=str.toLowerCase();
+	while(s<=e)
+		{
+			if(str.charAt(s)!=str.charAt(e))
+				return false;
+		}
+	return true;
 }
 module.exports = palindrome
